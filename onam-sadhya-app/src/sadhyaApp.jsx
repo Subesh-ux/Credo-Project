70b37723b911d1147690918e0bdfcc055ed0b380
Subesh-ux <subeshsubi7305@gmail.com>
2026-08-26 15:52:11 +0530

    created

---
+++ onam-sadhya-app/src/sadhyaApp.jsx
@@ -0,0 +1,14 @@
+import sadhyaFoodList from "../../assets/json/sadhyaFoodList.json";
+import { useState } from "react";
+export default function SadhyaFood(){
+    const [foodList,setFoodList] = useState(sadhyaFoodList)
+
+
+
+    return(
+        <>
+        
+        
+        </>
+    )
+}
